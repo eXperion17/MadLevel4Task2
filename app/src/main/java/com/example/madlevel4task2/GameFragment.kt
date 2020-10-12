6ab12cdae5adf0e8dcd63e8b2d3d1768fc9300dd
Marc Gomersbach <marc.gomersbach@hva.nl>
2020-10-12 16:03:30 +0200

    Renamed files & added the functionality inside of the layouts to support the display of the game results

--- app/src/main/java/com/example/madlevel4task2/GameFragment.kt
+++ app/src/main/java/com/example/madlevel4task2/GameFragment.kt
@@ -11,21 +11,21 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class GameFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_game, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        /*view.findViewById<Button>(R.id.button_first).setOnClickListener {
+            findNavController().navigate(R.id.action_gameFragment_to_historyFragment)
+        }*/
     }
 }
